42cf9b61aaf5799490ab7de0c1b5e7367058c413
Michael Bello <michaelayo360@gmail.com>
2026-05-03 13:10:46 +0000

    chore: set the correct loki stack chart version to the monitoring variable

--- terraform/modules/monitoring/variables.tf
+++ terraform/modules/monitoring/variables.tf
@@ -4,16 +4,9 @@ variable "prometheus_stack_chart_version" {
   default     = "70.4.2"
 }
 
-variable "loki_chart_version" {
-  description = "Helm chart version for Loki (standalone, monolithic mode)"
+variable "loki_stack_chart_version" {
+  description = "Helm chart version for loki-stack"
   type        = string
-  default     = "6.55.0"
-}
-
-variable "alloy_chart_version" {
-  description = "Helm chart version for Grafana Alloy (log collection agent)"
-  type        = string
-  default     = "0.12.5"
 }
 
 variable "grafana_admin_password" {
