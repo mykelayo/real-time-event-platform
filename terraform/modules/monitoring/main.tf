0deb3f6ccb806384b0cc2c385e871001662c17fc
Michael Bello <michaelayo360@gmail.com>
2026-05-03 13:09:47 +0000

    chore: set atomic and cleanup on fail to true

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -14,6 +14,8 @@ resource "helm_release" "kube_prometheus_stack" {
   version          = var.prometheus_stack_chart_version
   namespace        = kubernetes_namespace_v1.monitoring.metadata[0].name
   create_namespace = false
+  atomic           = true
+  cleanup_on_fail  = true
   wait             = true
   timeout          = 600
 
@@ -54,155 +56,36 @@ resource "helm_release" "kube_prometheus_stack" {
   depends_on = [kubernetes_namespace_v1.monitoring]
 }
 
-# Loki in monolithic / SingleBinary mode with filesystem storage.
-resource "helm_release" "loki" {
+resource "helm_release" "loki_stack" {
   name             = "loki"
   repository       = "https://grafana.github.io/helm-charts"
-  chart            = "loki"
-  version          = var.loki_chart_version
+  chart            = "loki-stack"
+  version          = var.loki_stack_chart_version
   namespace        = kubernetes_namespace_v1.monitoring.metadata[0].name
   create_namespace = false
-  wait             = true
-  timeout          = 600
+  atomic           = true
+  cleanup_on_fail  = true
+  wait             = false
+  timeout          = 300
 
   values = [yamlencode({
-  deploymentMode = "SingleBinary"
-
-  loki = {
-    commonConfig = {
-      replication_factor = 1
-    }
-    storage = {
-      type = "filesystem"
-    }
-    schemaConfig = {
-      configs = [
-        {
-          from         = "2024-04-01"
-          store        = "tsdb"
-          object_store = "filesystem"
-          schema       = "v13"
-          index = {
-            prefix = "index_"
-            period = "24h"
-          }
-        }
-      ]
-    }
-    auth_enabled = false
-  }
-
-  singleBinary = {
-    replicas = 1
-  }
-
-  read    = { replicas = 0 }
-  write   = { replicas = 0 }
-  backend = { replicas = 0 }
-
-  chunksCache = {
-    enabled = false
-  }
-
-  resultsCache = {
-    enabled = false
-  }
-
-  lokiCanary = {
-    enabled = false
-  }
-
-  test = {
-    enabled = false
-  }
-
-  monitoring = {
-    selfMonitoring = {
+    grafana = {
       enabled = false
-      grafanaAgent = {
-        installOperator = false
+    }
+    promtail = {
+      enabled = true
+    }
+    loki = {
+      persistence = {
+        enabled      = false
+        size         = "5Gi"
+        storageClass = ""
       }
     }
-  }
-
-  gateway = {
-    enabled = false
-  }
-})]
+  })]
 
   depends_on = [
     kubernetes_namespace_v1.monitoring,
     helm_release.kube_prometheus_stack,
   ]
-}
-
-# Grafana Alloy
-# Runs as a DaemonSet, collects pod logs and ships to Loki.
-resource "helm_release" "alloy" {
-  name             = "alloy"
-  repository       = "https://grafana.github.io/helm-charts"
-  chart            = "alloy"
-  version          = var.alloy_chart_version
-  namespace        = kubernetes_namespace_v1.monitoring.metadata[0].name
-  create_namespace = false
-  wait             = false
-  timeout          = 300
-
-  values = [yamlencode({
-    alloy = {
-      configMap = {
-        content = <<-ALLOY
-          discovery.kubernetes "pods" {
-            role = "pod"
-          }
-
-          discovery.relabel "pods" {
-            targets = discovery.kubernetes.pods.targets
-
-            rule {
-              source_labels = ["__meta_kubernetes_pod_node_name"]
-              target_label  = "__host__"
-            }
-
-            rule {
-              source_labels = ["__meta_kubernetes_namespace"]
-              target_label  = "namespace"
-            }
-
-            rule {
-              source_labels = ["__meta_kubernetes_pod_name"]
-              target_label  = "pod"
-            }
-
-            rule {
-              source_labels = ["__meta_kubernetes_pod_container_name"]
-              target_label  = "container"
-            }
-
-            rule {
-              source_labels = ["__meta_kubernetes_pod_label_app"]
-              target_label  = "app"
-            }
-          }
-
-          loki.source.kubernetes "pods" {
-            targets    = discovery.relabel.pods.output
-            forward_to = [loki.write.default.receiver]
-          }
-
-          loki.write "default" {
-            endpoint {
-              url = "http://loki.monitoring:3100/loki/api/v1/push"
-            }
-          }
-        ALLOY
-      }
-    }
-
-    controller = {
-      type = "daemonset"
-    }
-  })]
-
-  depends_on = [helm_release.loki]
 }
