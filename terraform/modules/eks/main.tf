9446ae5a6113a2ceb6823ebe19ba9e11391eafe0
Michael Bello <michaelayo360@gmail.com>
2026-05-03 13:08:02 +0000

    chore: remove the cluster create admin permission & set up an aws eks access entry for the eks cluster

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -16,7 +16,6 @@ module "eks" {
   ]
 
   enable_irsa                              = true
-  enable_cluster_creator_admin_permissions = true
 
   cloudwatch_log_group_retention_in_days = 30
   cloudwatch_log_group_class             = "STANDARD"
@@ -149,28 +148,20 @@ resource "helm_release" "aws_load_balancer_controller" {
   ]
 }
 
-# These resources are disabled because enable_cluster_creator_admin_permissions = true
-# in the EKS module handles admin access automatically
-/* data "aws_caller_identity" "current" {}
-
 resource "aws_eks_access_entry" "admin" {
   cluster_name  = module.eks.cluster_name
-  principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/michael-devops"
+  principal_arn = var.admin_role_arn
   type          = "STANDARD"
-
-  depends_on = [module.eks]
 
   tags = var.tags
 }
 
 resource "aws_eks_access_policy_association" "admin" {
   cluster_name  = module.eks.cluster_name
-  principal_arn = aws_eks_access_entry.admin.principal_arn
+  principal_arn = var.admin_role_arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
-  depends_on = [aws_eks_access_entry.admin]
-  
   access_scope {
     type = "cluster"
   }
-} */
+}
